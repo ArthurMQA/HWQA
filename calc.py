f1698a0170f0cecb46b001afbca8e0ad899f0958
ArthurMQA <avmazikin@gmail.com>
2020-03-13 14:45:05 +0300

    Calc ver. 4.0

--- calc.py
+++ calc.py
@@ -1,23 +1,39 @@
 '''
-Calcultor v. 0.1
+Calcultor v. 0.4
 '''
 
 def input_number():
     num = input("Введите число: ")
+    try:
+        num = float(num)
+    except ValueError:
+        num = input_number()
     return num
 
 def input_oper():
-    oper = input("Операция('*','/', '+', '-'): ")
+    oper = input("Операция(*, /, +, -): ")
     return oper
 
 def calc_me(x,y, oper):
     if oper == '*':
         return x * y
     elif oper == '/':
-        return x / y
+        if y == 0:
+            return "ERROR: Divide by zero!"
+        else:
+            return x / y
     elif oper == '+':
         return x + y
     elif oper == '-':
         return x - y
     else:
-        return "Wrong operation"
+        return "ERROR: Uknow operation"
+
+def body():
+    number1 = input_number()
+    oper = input_oper()
+    number2 = input_number()
+    result = calc_me(number1,number2, oper)
+    print(result)
+if __name__ == '__main__':
+    body()
